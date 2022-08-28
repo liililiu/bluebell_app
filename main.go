b68e529d711d835d48cea18150924f1ad44db39e
liililiu <liililiu007@gmail.com>
2022-08-28 10:42:00 +0800

    viper.init&&zap.init&&mysql.init&&redis.init

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bluebell_app/dao/mysql"
+	"bluebell_app/dao/redis"
 	"bluebell_app/logger"
 	"bluebell_app/settings"
 	"fmt"
+
+	"go.uber.org/zap"
 )
 
 //GoWeb开发通用脚手架
@@ -19,8 +23,18 @@ func main() {
 		fmt.Printf("init logger failed , err: %v", err)
 		return
 	}
+	zap.L().Info("logger init success...")
+
 	//3.初始化mysql
+	if err := mysql.Init(); err != nil {
+		fmt.Printf("init mysql failed , err: %v", err)
+		return
+	}
 	//4.初始化redis
+	if err := redis.Init(); err != nil {
+		fmt.Printf("init redis failed , err: %v", err)
+		return
+	}
 	//5.注册路由
 	//6.启停服务（优雅关机）
 }
